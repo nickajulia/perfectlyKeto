db82e4cacda4bcd3eed2df277715e2b5752c5f9e
Youssef Salama <youssefhanii@hotmail.com>
2018-02-06 23:41:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const redis = require("redis");
+const client = redis.createClient(process.env.REDIS_URL);
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(process.env.PORT, () => console.log('App started!'))
