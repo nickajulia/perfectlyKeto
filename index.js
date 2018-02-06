00f4e7d5a66baa9e6fbd537f064bc5b8dee5cf4f
Youssef Salama <youssefhanii@hotmail.com>
2018-02-06 23:47:24 +0200

    test 2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const redis = require("redis");
 const client = redis.createClient(process.env.REDIS_URL);
 const app = express();
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
