6e98238bc06e909232cb9b32acc0588f24490b50
Youssef Salama <youssefhanii@hotmail.com>
2018-02-07 20:13:41 +0200

    test

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const redis = require("redis");
 const client = redis.createClient(process.env.REDIS_URL);
+///DIALOG FLOW INIT
+var apiai = require('apiai');
+var app = apiai(process.env.ACCESS_TOKEN_APIAI);
+
+
+///END DIALOG FLOW
+
+
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/webhook', (req, res) => {
+    console.log(req);
+
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(process.env.PORT, () => console.log('App started!'))
+app.listen(process.env.PORT, () => console.log('App started!'));
