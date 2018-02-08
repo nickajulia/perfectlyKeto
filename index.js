2f2de364bc241ab03e2e4e4a010eeb45df073afc
Youssef Salama <youssefhanii@hotmail.com>
2018-02-08 03:22:34 +0200

    test

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const allData = [];
 
 app.get('/', (req, res) => {
     doc.getRows(1, {}, function(err, rows) {
-        console.log('Read ' + rows + ' rows');
+        console.log('Read ' + err + ' rows');
 
     })
     res.send('Updated!')
